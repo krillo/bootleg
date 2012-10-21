d66f38be2db55274acb6e48ed9eb093e7594db88
Kristian Erendi <krillo@gmail.com>
2012-10-21 16:21:02 +0200

    single h1

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <div class="row-fluid">
       <div class="span12 rep-post">          
-        <?php the_title(); ?>
+        <h1><?php the_title(); ?></h1>
         <?php the_content(); ?>
         <?php edit_post_link(); ?>
       </div>  
